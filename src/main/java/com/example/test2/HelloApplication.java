528e83e232732539ee9f1cde5f6df2aab65dadd9
Denis <samparizona2018@yandex.ru>
2022-09-12 22:54:06 +0300

    ww

--- src/main/java/com/example/test2/HelloApplication.java
+++ src/main/java/com/example/test2/HelloApplication.java
@@ -1,25 +1,35 @@
 package com.example.test2;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @FXML
-    TextField textField;
+
+    @FXML TextField input;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("firstscene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 350);
-        stage.setTitle("Полный квадрат");
-        stage.setScene(scene);
-        stage.getIcons().add(new Image("file:icon.png"));
-        stage.show();
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("firstscene.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 350);
+            stage.setTitle("Полный квадрат");
+            stage.setScene(scene);
+            stage.getIcons().add(new Image("file:icon.png"));
+            stage.show();
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/test2/HelloController.java
+++ src/main/java/com/example/test2/HelloController.java
@@ -2,29 +2,57 @@ package com.example.test2;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.lang.reflect.Field;
 
-public class HelloController {
+public class HelloController extends ActionEvent {
 
-    @FXML TextField textField;
     @FXML
-    Label output;
-    public void solveOnClick(ActionEvent actionEvent) {;
-        String cc = textField.getText();
-        try{
+    Label outputText;
+    @FXML
+    TextField input;
+    @FXML
+    Label errorOutput;
+
+
+    public void solveOnClick(ActionEvent actionEvent) throws IOException {
+        try {
+            String cc = input.getText();
             int value = Integer.parseInt(cc);
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Pane root = fxmlLoader.load(getClass().getResource("secondscene.fxml").openStream());
+            Scene scene = new Scene(root, 600, 350);
+            stage.setScene(scene);
+            stage.setTitle("Полный квадрат");
+            stage.getIcons().add(new Image("file:icon.png"));
+            errorOutput.setText("");
+            Label label = (Label) scene.lookup("#outputText");
+
             if(checkForSqrt(value)){
-                output.setText("Число " + value + " является полным квадратом.");
+                label.setText("Число " + value + " является полным квадратом.");
             }
-            else{
-                output.setText("Число " + value + " не является полным квадратом.");
+            else if (!checkForSqrt(value)){
+                label.setText("Число " + value + " не является полным квадратом.");
             }
-        }
-        catch (Exception ex){
-            output.setText("Введены некорректные данные.");
+            stage.showAndWait();
+        } catch (Exception ex) {
+            errorOutput.setText("Ошибка ввода данных.");
         }
     }
+
+
+
     public static boolean checkForSqrt(int x){
         return x % Math.sqrt(x) == 0;
     }
